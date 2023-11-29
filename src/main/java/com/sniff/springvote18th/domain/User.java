3281a00ee196ae8c91156c0450ed7dac9fc27469
yoonsseo <yellisa2000@gmail.com>
2023-11-30 07:06:16 +0900

    [Feat] UserDetails, Role 수정 #3

--- src/main/java/com/sniff/springvote18th/domain/User.java
+++ src/main/java/com/sniff/springvote18th/domain/User.java
@@ -1,28 +1,46 @@
 package com.sniff.springvote18th.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
+    @NotNull
     private String loginId;
+
+    @NotNull
     private String password;
+
+    @NotNull
     private String email;
+
+    @NotNull
     private String name;
 
     @Enumerated(EnumType.STRING)
+    @NotNull
     private TeamName teamName;
 
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Part part;
 
     @Column(columnDefinition = "TINYINT(0) default 0")
@@ -32,5 +50,47 @@ public class User {
     private boolean isTeamVoted;
 
     @Enumerated(EnumType.STRING)
+    @ColumnDefault("\"USER\"")
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auth = new ArrayList<>();
+        auth.add(new SimpleGrantedAuthority(this.role.name()));
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.loginId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        //계정이 만료됐는지 리턴 -> true는 완료되지 않음 의미
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        //계정이 잠겨있는지 리턴 -> true는 잠기지 않음
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        //비밀번호가 만료됐는지 리턴 -> true는 만료X 의미
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        //계정이 활성화돼 있는지 리턴 -> true는 활성화 상태 의미
+        return true;
+    }
 }
--- src/main/java/com/sniff/springvote18th/repository/UserRepository.java
+++ src/main/java/com/sniff/springvote18th/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.sniff.springvote18th.repository;
 
 import com.sniff.springvote18th.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginId(String loginId);
 
     Optional<User> findByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.isTeamVoted = true WHERE u.id = :userId")
+    void updateIsTeamVoted(@Param("userId") Long userId);
 }
