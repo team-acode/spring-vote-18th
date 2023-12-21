80c84f428d8f32d00e0916718e1b27a81f94a3f3
‘yj-leez’ <yj_leez@naver.com>
2023-12-21 20:40:24 +0900

    [Fix] cors 에러 해결

--- src/main/java/com/sniff/springvote18th/config/SecurityConfig.java
+++ src/main/java/com/sniff/springvote18th/config/SecurityConfig.java
@@ -26,21 +26,49 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic(HttpBasicConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(httpBasic -> httpBasic.disable()) // http 기본 인증 비활성화
-                .formLogin(formLogin -> formLogin.disable()) // 사용자 지정 로그인 로직 구현
+//        return http
+//                .httpBasic(HttpBasicConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .httpBasic(httpBasic -> httpBasic.disable()) // http 기본 인증 비활성화
+//                .formLogin(formLogin -> formLogin.disable()) // 사용자 지정 로그인 로직 구현
+//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Preflight Request 허용해주기
+//                        .requestMatchers("/api/*/users/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET).permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/**").authenticated())
+//                        .sessionManagement((sessionManagement) -> sessionManagement
+//                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+//                        .build();
+        http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Preflight Request 허용해주기
-                        .requestMatchers("/api/*/users/**").permitAll()
-                        .requestMatchers(HttpMethod.GET).permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/**").authenticated())
-                        .sessionManagement((sessionManagement) -> sessionManagement
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-                        .build();
+                .authorizeRequests() // 요청에 대한 접근 권한을 설정하는 메서드
+                .requestMatchers("/api/*/users/**").permitAll()
+                .requestMatchers(HttpMethod.GET).permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/**").authenticated()
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll(); // Preflight Request 허용해주기
+
+        http
+                .csrf(csrf -> csrf.disable())// token을 사용하는 방식이기 때문에 csrf를 disable로 설정
+                .formLogin(formLogin -> formLogin.disable()) // 사용자 지정 로그인 로직 구현
+                .httpBasic(httpBasic -> httpBasic.disable()); // http 기본 인증 비활성화
+
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        // 세션을 사용하지 않기 때문에 STATELESS로 설정
+        http.sessionManagement(sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // enable Mysql-console
+        http.headers(headers ->
+                headers.frameOptions(options ->
+                        options.sameOrigin()
+                )
+        );
+        return http.build();
+
+
     }
 
 
